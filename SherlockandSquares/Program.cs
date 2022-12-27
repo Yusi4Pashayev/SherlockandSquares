33020c44ab70af0e0e5eb4ae470a9a457954e1a6
yusi4 <yusi4@DESKTOP-E80JV74>
2022-12-28 03:51:50 +0400

    Add project files.

---
+++ SherlockandSquares/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SherlockandSquares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("araligi mueyyen edin:");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int r, c, sum = 0;
+            
+            if (a > b) 
+            { 
+                r = a;
+                a = b;
+                b = r;
+            }
+            for (int i = 1; i > 0; i++)
+            {
+                c = (int)Math.Pow(i, 2);
+                if (c >= a && c <= b)
+                {
+                    sum++;
+                }
+                else if (c >= b) break; 
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
